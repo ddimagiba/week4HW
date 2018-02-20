dfc1f499cbb504f9f133c08571a9943bf2f66d39
ddimagiba <david.dimagiba@gmail.com>
2018-02-19 19:41:40 -0500

    change
    Not sure what I am doing

---
+++ HWpart2/main.js
@@ -0,0 +1,17 @@
+console.log('main.js is connected!')
+
+var cel = 1.8
+var temp1 = celsius 
+var temp2 = faranheit 
+
+
+
+
+
+
+var convertor = {
+	multiply: function(cel, temp1)
+		return 1.8 * temp1
+
+	add: function( )	
+}
---
+++ movies-and-jurassic-park.js
@@ -0,0 +1,109 @@
+console.log('movies-and-jurassic-park.js is connected! woohoo!');
+
+// Part 1
+
+var snakewaterMontana = {
+  paleontologist: "Dr. Alan Grant",
+  depth: "10 meters",
+  specimen: "Velociraptor"
+};
+
+
+// Do the tasks down here!
+
+var fightclub = {
+  director: "David Fincher"
+  actor: "Edward Norton, Brad Pitt"
+  releaseYear: 1999,
+  duration: 120
+  extendedCut: 25
+
+}
+
+print director(David Fincher)
+print releaseYear(1999)
+if duration > 120 {
+  extendedCut: "25";
+
+var guestOfHonor = Dr. Alan Grant
+var home = snakewaterMontana
+
+console.log(paleontologist);
+console.log(snakewaterMontana);
+
+var cleverGirl = Velociraptor
+
+console.log(Velociraptor)
+
+
+
+
+}
+
+
+
+// Part 2
+
+var nicaragua = {
+  depth: "200 meters",
+  annual_budget: 1500000,
+  specimens: [
+    "Tyrannosaurus Rex",
+    "Stegosaurous",
+    "Triceratops",
+    "Velociraptor"
+  ],
+};
+
+// Do the tasks down here!
+var specimens(Tyrannosaurus Rex + Stegosaurous + Triceratops + Velociraptor);
+var FavoriteSpecimen(Tyrannosaurus Rex[nicaragua]);
+var annual_budget = 250000
+
+
+
+
+// Part 3
+
+var hammondsMines = {
+  buenos_aires: {
+    depth: "400 meters",
+    annual_budget: 1000000,
+    specimens: [
+      "Dilophosaurus",
+      "Brachiosaurus"
+    ],
+  },
+  mexico: {
+    depth: "350 meters",
+    annual_budget: 900000,
+    specimens: [
+      "Gallimimus",
+      "Parasaurolophus"
+    ],
+  },
+};
+
+// Do the tasks down here!
+
+var location(mexico);
+var depth(350 meters);
+
+var specimens(Dilophosaurus + Brachiosaurus)
+console.log(specimens)
+
+console.log(hammondsMines): innerHtml: nicaragua
+
+var annual_budget = {
+  buenos_aires: 1000000,
+  mexico: 90000,
+}
+
+var trackbudget(buenos_aires + mexico);
+console.log (100000 + 90000)
+
+var specimens(Parasaurolophus)
+console.log(location,'mexico')
+
+
+
