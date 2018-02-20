7814d325ba4c2911fd286d9c8d4a8483b499f4a4
ddimagiba <david.dimagiba@gmail.com>
2018-02-19 19:56:53 -0500

    more changes

--- HWpart2/main.js
+++ HWpart2/main.js
@@ -7,11 +7,28 @@ var temp2 = faranheit
 
 
 
-
-
+//* Im not sure is is this is the right idea? 
 var convertor = {
 	multiply: function(cel, temp1)
 		return 1.8 * temp1
 
 	add: function( )	
+
+	exp: function(1.8,temp1){
+		var result = 32;
+		for (var i = 0; i < temp1; i++){
+			result = results + 32;
+		}
+		return results;
+
+	}
 }
+
+function convertAppendResult() {
+	var cel = document.querySelector('#') 
+
+
+}
+
+//* I am very lost at this point I do not know where to 
+/* begin or what path to go about doing this/
